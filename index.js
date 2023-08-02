01056e30620f28f4450749abd53fd7da85eb1d72
Akash <kumawat.aakash515@gmail.com>
2023-08-02 21:01:33 +0530

    starts

---
+++ index.js
@@ -0,0 +1,14 @@
+class computer{
+    constructor(name) {
+        this.name = name
+    }
+
+    return() {
+        console.log("the computer is running")
+    }
+}
+
+class macbook extends computer {
+    constructor
+}
+
