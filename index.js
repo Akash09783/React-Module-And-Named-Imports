95abc53af2bd09e3d319a6302d072d2e534011e4
Akash <kumawat.aakash515@gmail.com>
2023-08-02 21:13:22 +0530

    Understanding Modules

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ class computer{
 }
 
 class macbook extends computer {
-    constructor
+    constructor(name, company) {
+        super(name)
+        this.company = company
+    }
+    logIn() {
+        console.log("you are logged into a macbook" + this.name)
+    }
 }
+
+const macbook_air = new macbook('air', 'apple');
+macbook_air.logIn()
+macbook_air.return()
+f
 
